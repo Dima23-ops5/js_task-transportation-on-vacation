74d66998ff213cf6b5e5a5373402001cf4c73424
Dmytro Makhanok <dmmhnk23@gmail.com>
2026-04-30 23:33:32 +0200

    remove all hardcode value from a code

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,14 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const dailyRate = 40;
+  const DAILY_RATE = 40;
+  const SEVEN_DAYS = 7;
+  const THREE_DAYS = 3;
+  const DISCOUNT_7_DAYS = 50;
+  const DISCOUNT_3_TO_6_DAYS = 20;
 
-  let totalCost = days * dailyRate;
+  let totalCost = days * DAILY_RATE;
 
-  if (days >= 7) {
-    totalCost -= 50;
-  } else if (days >= 3 && days < 7) {
-    totalCost -= 20;
+  if (days >= SEVEN_DAYS) {
+    totalCost -= DISCOUNT_7_DAYS;
+  } else if (days >= THREE_DAYS && days < SEVEN_DAYS) {
+    totalCost -= DISCOUNT_3_TO_6_DAYS;
   }
 
   return totalCost;
